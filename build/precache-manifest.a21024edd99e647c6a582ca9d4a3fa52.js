d1a87327b560a5eb70e4ca225d460a357f53ee3b
Amrez <amrezb2c@gmail.com>
2023-06-07 02:35:00 +0500

    build

---
+++ build/precache-manifest.a21024edd99e647c6a582ca9d4a3fa52.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e237eb984e18559ac4bd",
+    "url": "/static/js/main.3569fd52.chunk.js"
+  },
+  {
+    "revision": "8a31ed3f3efa8b72f2c5",
+    "url": "/static/js/runtime~main.6d5d678e.js"
+  },
+  {
+    "revision": "da45ce9a646cb331bf07",
+    "url": "/static/css/2.f7c8e1bc.chunk.css"
+  },
+  {
+    "revision": "da45ce9a646cb331bf07",
+    "url": "/static/js/2.1ff1b994.chunk.js"
+  },
+  {
+    "revision": "02c234452e77aaebc1eb",
+    "url": "/static/js/3.808c7236.chunk.js"
+  },
+  {
+    "revision": "ded2b771c2872ff661be",
+    "url": "/static/js/4.872d57de.chunk.js"
+  },
+  {
+    "revision": "ba52db67c2f1fb1666bd",
+    "url": "/static/js/5.9a8930a0.chunk.js"
+  },
+  {
+    "revision": "5473206477b5467bc119",
+    "url": "/static/js/6.218dc5f9.chunk.js"
+  },
+  {
+    "revision": "e0f29b86a8b9d663853d",
+    "url": "/static/js/7.8fc253de.chunk.js"
+  },
+  {
+    "revision": "1d5704eb311cb1ddfeae",
+    "url": "/static/js/8.da2d202e.chunk.js"
+  },
+  {
+    "revision": "a3c19977d523c5389edb",
+    "url": "/static/js/9.36a06055.chunk.js"
+  },
+  {
+    "revision": "cbac1ba8fec8e8ca9ee9",
+    "url": "/static/js/10.a2c2d18a.chunk.js"
+  },
+  {
+    "revision": "026c7819329b57dbf1a3",
+    "url": "/static/js/11.2dceb82f.chunk.js"
+  },
+  {
+    "revision": "398a6fa0902f42b38c7dad067cf1e540",
+    "url": "/static/media/no_image.398a6fa0.jpg"
+  },
+  {
+    "revision": "7f9bf543263d576903897e57671be03a",
+    "url": "/index.html"
+  }
+];
